411af6f2ac7ab48b9908133a2237ea878576fb61
we9lii <wesli808@gmail.com>
2026-06-22 18:41:28 +0000

    Create src/utils/hotkey_manager.py

---
+++ src/utils/hotkey_manager.py
@@ -0,0 +1,13 @@
+import keyboard
+from utils.logger import log_info
+
+class HotkeyManager:
+    def __init__(self):
+        self.hotkeys = {}
+    
+    def register_hotkey(self, key, callback):
+        keyboard.add_hotkey(key, callback)
+        log_info(f"Registered hotkey: {key}")
+    
+    def unregister_all(self):
+        keyboard.unhook_all()
